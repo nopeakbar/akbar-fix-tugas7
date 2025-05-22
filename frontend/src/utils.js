f51725769fabc70e2a1d55d1fcbb50862582999e
nopeakbar <114820984+nopeakbar@users.noreply.github.com>
2025-05-22 16:39:46 +0700

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,7 +1,7 @@
 // src/utils.js
 import axios from 'axios';
 
-export const BASE_URL = 'https://backend-559917148272.us-central1.run.app';
+export const BASE_URL = 'https://be-akbar-tugas7-559917148272.us-central1.run.app';
 
 export const API = axios.create({
   baseURL: BASE_URL,
